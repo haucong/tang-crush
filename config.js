6b6a871e7fe5ab1d1e9083cbeecf2aa8f9ef5873
haucong <87372478+haucong@users.noreply.github.com>
2021-07-20 20:19:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
     introTitle: 'Hân ơii!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    introDesc: `Nói nghe này nè..
+    muốn nghe hôn?^^ `,
+    btnIntro: 'hihi dạ',
+    title: 'Nắng đã có mũ, mưa đã có ô, còn em sẽ có ai? 😙',
+    desc: 'Em xinh đẹp ơi..em có muốn làm con dâu của mẹ anh không.. ',
     btnYes: 'Em vẫn thương anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối ngủ ngoan nha cục vàng ^^',
-    btnAccept: 'Dạaaa <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messDesc: 'Tối ngủ ngoan nha cục vàng <3 ',
+    btnAccept: 'Dạaaa ^^ <3',
+    messLink: 'https://www.facebook.com/bapconghau/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}y
