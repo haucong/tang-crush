1a562a0839e60a59f520040fb12d0263893c8725
haucong <87372478+haucong@users.noreply.github.com>
2021-07-26 21:24:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     introTitle: 'Hân ơii!',
     introDesc: `Nói nghe này nè..
-    muốn nghe hôn?^^ `,
-    btnIntro: 'hihi dạ',
-    title: 'Nắng đã có mũ, mưa đã có ô, còn em sẽ có ai? 😙',
-    desc: 'Em xinh đẹp ơi..em có muốn làm con dâu của mẹ anh không.. ',
-    btnYes: 'Em vẫn thương anh <33',
+     `,
+    btnIntro: 'dạ',
+    title: '',
+    desc: 'em có muốn làm con dâu của mẹ anh không.. ',
+    btnYes: 'dạ có <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'em vẫn cho a cơ hội mà đúng hong <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'dạ.. <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối ngủ ngoan nha cục vàng <3 ',
-    btnAccept: 'Dạaaa ^^ <3',
+    messDesc: 'Tối ngủ ngoan nha em <3 ',
+    btnAccept: 'Dạaaa anh^^ <3',
     messLink: 'https://www.facebook.com/bapconghau/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
